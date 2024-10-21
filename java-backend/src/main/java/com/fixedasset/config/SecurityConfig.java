d3aa03bd7477a3ba0ac75515995fc1bfb5b082a0
Felix <53119101+felix9611@users.noreply.github.com>
2024-10-21 10:25:43 -0400

    open api doc config

--- java-backend/src/main/java/com/fixedasset/config/SecurityConfig.java
+++ java-backend/src/main/java/com/fixedasset/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -56,12 +57,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return encoder;
     }
 
+    @Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/swagger-ui/**", "/v3/api-docs/**");
+	}
 
     private static final String[] URL_WHITELIST = {
-            "/login",
-            "/logout",
-            "/captcha",
-            "/favicon.ico",
+        "/login",
+        "/logout",
+        "/captcha",
+        "/favicon.ico",
+        "/swagger-ui/**",
+        "/swagger-ui/index.html",
+        "/swagger-ui.html",
+         "/swagger.json/**"
     };
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
