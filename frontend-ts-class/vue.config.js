24f53b34f281e6fb888d085ded3c7d82e6ded4d0
Felix <53119101+felix9611@users.noreply.github.com>
2025-02-07 19:15:33 -0500

    allow

--- frontend-ts-class/vue.config.js
+++ frontend-ts-class/vue.config.js
@@ -16,7 +16,7 @@ module.exports = {
         proxy: {
             [process.env.VUE_APP_BASE_API]: {
                 //后端服务地址和端口
-                target: 'http://localhost:8370',
+                target: 'https://fixedasset-prod-felix9611-d3fc9544.koyeb.app',
                 //是否跨域
                 changeOrigin: true,
                 pathRewrite: {
